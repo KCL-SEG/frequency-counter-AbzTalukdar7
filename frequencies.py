94eb3bf85d7dc112913833d0de855b3f2143caa0
Abed Talukdar <talukdar.a@hotmail.com>
2022-10-20 17:04:11 +0100

    complete frequency task

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(len(items)):
+        count = 0
+        for j in range(len(items)):
+            if str(items[i]) == str(items[j]):
+                count += 1;
+        frequencies.update({str(items[i]):count})
     return frequencies
